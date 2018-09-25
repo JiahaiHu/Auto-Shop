f5b7d6abd9eee6079ea7092de2b09cf58f1c067d
Alex Hu <892220879@qq.com>
2018-09-25 09:22:20 +0800

    add another test

--- FSM_sim.v
+++ FSM_sim.v
@@ -17,32 +17,51 @@ module FSM_sim;
     end
     
     initial begin
-        // // test s1
-        // #10 coin_val = 2'b01;
-        // insert = 1;
-        // #20 coin_val = 2'b00;
-        // insert = 0; // insert 1
+        // test s1
+        // insert 1
+        #10 coin_val = 2'b01;
+        insert = 1;
+        #20 coin_val = 2'b00;
+        insert = 0;
+        // cancel
+        #20 cancel_flag = 1;
+        #20 cancel_flag = 0;
 
+        // test s2
+        // insert 1 for 3 times
+        #60 coin_val = 2'b01;
+        insert = 1;
+        #100 coin_val = 2'b00;
+        insert = 0;
+        // choose drink 1
+        #20 drink_op = 1 ;
+        #20 drink_op = 0 ;
+        // cancel
         // #20 cancel_flag = 1;
         // #20 cancel_flag = 0;
 
-        // //test s2
-        // #60 coin_val = 2'b01;
-        // insert = 1;
-        // #100 coin_val = 2'b00;
-        // insert = 0; // insert 3
-
-        // #20 drink_op = 1 ;
-        // #20 drink_op = 0 ;
-
-        // // test s3
-        #10 coin_val = 2'b10;
+        // test s3
+        // insert 10
+        #60 coin_val = 2'b10;
         insert = 1;
         #20 coin_val = 2'b00;
-        insert = 0; // insert 10
-
+        insert = 0;
+        // choose drink 2
         #20 drink_op = 2;
         #20 drink_op = 0;
+        // cancel
+        // #20 cancel_flag = 1;
+        // #20 cancel_flag = 0;
+
+        // test
+        // insert 1 for 5 times
+        #60 coin_val = 2'b01;
+        insert = 1;
+        #180 coin_val = 2'b00;
+        insert = 0;
+        // choose drink 2
+        #20 drink_op = 2;
+        #20 drink_op = 2;
     end
     
     FSM test(
@@ -58,4 +77,5 @@ module FSM_sim;
         charge_ind,     // 取零钱或退币指示
         coin_sum        // 投币总值或退币总值(Q1型定点小数:q=x*2)
     );
+
 endmodule
